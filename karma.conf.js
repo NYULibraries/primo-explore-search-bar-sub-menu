f7aa767d401644e748e50d535b117f211f827a26
Elliott Grieco <elliott.grieco@gmail.com>
2018-03-20 17:12:04 -0400

    Removes redundant files config

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,6 @@ module.exports = function(config) {
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'js/**/*.js',
-      'spec/**/*.js',
       'spec/**/*.spec.js'
     ],
     preprocessors: {

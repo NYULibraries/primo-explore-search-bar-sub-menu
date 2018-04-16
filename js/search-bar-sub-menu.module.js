ee1d9281a49087ea340656cbb0b6436b5b4e44c0
Elliott Grieco <elliott.grieco@gmail.com>
2018-04-16 14:00:09 -0400

    modularization and coveralls

--- js/search-bar-sub-menu.module.js
+++ js/search-bar-sub-menu.module.js
@@ -1,27 +1,19 @@
+import searchBarSubMenuTemplate from '../html/searchBarSubMenu.html';
+
 angular
   .module('searchBarSubMenu', [])
   .controller('searchBarSubMenuController', ['searchBarSubMenuItems', '$scope', '$filter', function(items, $scope, $filter) {
     this.$onInit = () => {
       $scope.items = items;
-    }
+    };
     $scope.translate = (original) => {
       return original.replace(/\{(.+)\}/g, (match, p1) => $filter('translate')(p1));
-    }
+    };
     $scope.goToUrl = (url) => {
       window.open(url, '_blank');
-    }
+    };
   }])
   .component('searchBarSubMenu', {
     controller: 'searchBarSubMenuController',
-    template: `<div class="layout-align-end-center layout-row flex search-bar-sub-menu">
-                <ul>
-                  <li ng-repeat="item in items">
-                  <button data-href="{{ translate(item.action) }}" aria-label="{{ translate(item.description) }}" ng-click="goToUrl(translate(item.action))" class="button-with-icon zero-margin md-button md-small {{item.cssClasses}}" type="button">
-                    <md-tooltip md-direction="bottom" md-delay="500">{{ translate(item.description) }}</md-tooltip><prm-icon style="z-index:1" icon-type="svg" svg-icon-set="{{item.icon.set}}" icon-definition="{{item.icon.icon}}"></prm-icon>
-                    <span class="search-bar-sub-menu-item" ng-class="(item.show_xs) ? '' : 'hide-xs'">{{ translate(item.name) }}</span>
-                  </button>
-                  </li>
-                </ul>
-              </div>`
-
+    template: searchBarSubMenuTemplate
   });

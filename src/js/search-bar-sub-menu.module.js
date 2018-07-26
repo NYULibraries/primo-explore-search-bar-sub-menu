64cb420fc80fbf054863a2b99bd747a922c65ee3
Elliott Grieco <elliott.grieco@nyu.edu>
2018-07-26 14:02:06 -0400

    Fixes translation bug

--- src/js/search-bar-sub-menu.module.js
+++ src/js/search-bar-sub-menu.module.js
@@ -7,7 +7,7 @@ angular
       $scope.items = items;
     };
     $scope.translate = (original) => {
-      return original.replace(/\{(.+)\}/g, (match, p1) => $filter('translate')(p1));
+      return original.replace(/\{(.+?)\}/g, (match, p1) => $filter('translate')(p1));
     };
     $scope.goToUrl = (url) => {
       window.open(url, '_blank');
